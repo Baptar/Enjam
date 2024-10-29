ea8ff64db80faf3950fda091a00fc98f55b2e7e4
Baptar <ruberte43@gmail.com>
2024-10-29 19:08:20 +0100

    add controller and modif scene

---
+++ Assets/Script/FPSController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float walkSpeed = 3f;
+    
+    
+    public float lookSpeed = 2f;
+    
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0f;
+    
+    public bool canMove = true;
+
+    CharacterController characterController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Handles Movement
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        
+        float curSpeedX = walkSpeed * Input.GetAxis("Vertical");
+        float curSpeedY = walkSpeed * Input.GetAxis("Horizontal");
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        
+        #endregion
+        
+        #region Handles Rotation
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X")* lookSpeed, 0);
+        }
+        
+        #endregion
+    }
+}
