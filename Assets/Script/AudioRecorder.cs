7f24493765fc8e3c2f309fdcf64111f3144c8830
Matéo Avventuriero <mateo.aven@orange.fr>
2024-10-30 16:47:02 +0100

    Add recording zone

--- Assets/Script/AudioRecorder.cs
+++ Assets/Script/AudioRecorder.cs
@@ -4,46 +4,21 @@ using UnityEngine;
 
 public class AudioRecorder : MonoBehaviour
 {
-    
-    private AudioClip recordedClip;
-    [SerializeField] AudioSource audioSource;
-    
-    public void StartRecording()
-    {
-        string device = Microphone.devices[0];
-        int sampleRate = 44100;
-        int length = 3599;
-        
-        recordedClip = Microphone.Start(device, false, length, sampleRate);
-    }
 
-    public void PlayRecording()
-    {
-        audioSource.clip = recordedClip;
-        audioSource.Play();
-    }
-    
-    public void StopRecording()
-    {
-        Microphone.End(Microphone.devices[0]);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("MIC LENGTH: " + Microphone.devices.Length);
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            StartRecording();
-        }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            PlayRecording();
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            StopRecording();
-        }
-    }
+   public void OnTriggerEnter(Collider collision)
+   {
+      if (collision.gameObject.tag == "Player")
+      {
+         Debug.Log("RECORDING");
+      }
+   }
+   
+   public void OnTriggerExit(Collider collision)
+   {
+      if (collision.gameObject.tag == "Player")
+      {
+         Debug.Log("STOP RECORDING");
+      }
+   }
+   
 }
