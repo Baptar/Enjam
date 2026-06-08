6fbd27e7bcfd7379fdf78536c64c1f8b87164ebe
Baptiste Ruberte <ruberte43@gmail.com>
2026-06-08 21:18:47 +0200

    end door non euclidian portals

--- Assets/Project/Scripts/Interactable/SpecificCase/FinalDoor.cs
+++ Assets/Project/Scripts/Interactable/SpecificCase/FinalDoor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FinalDoor : ObjectInteractable
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator animatorEndDoor;
     
     private Transform playerCameraTransform;
     private Transform targetTransform;
@@ -16,9 +17,12 @@ public class FinalDoor : ObjectInteractable
             SetInteractable(false);
             if (!eventSoundOnInteract.IsNull) PlaySound(eventSoundOnInteract);
             
-            bool res = CheckIsInFront();
+            /*bool res = CheckIsInFront();
             
-            animator.Play(res ? "Door Opened" : "Door Opened Reverse", 0, 0f);
+            animator.Play(!res ? "Door Opened" : "Door Opened Reverse", 0, 0f);
+            animatorEndDoor.Play(!res ? "Door Opened" : "Door Opened Reverse", 0, 0f);*/
+            animator.Play("Door Opened", 0, 0f);
+            animatorEndDoor.Play("Door Opened", 0, 0f);
             
             eventOnInteract?.Invoke();
         }
