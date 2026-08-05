e05ff8312b4c0a7f89b4b4625637970db4d755a3
Matéo Avventuriero <77232541+Proutymat@users.noreply.github.com>
2026-08-05 04:35:52 +0200

    feat: add SteamManager to unlock success

---
+++ Assets/Project/Scripts/SteamManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using Steamworks;
+
+public class SteamManager : MonoBehaviour
+{
+    public static bool Initialized { get; private set; }
+    
+    private void Awake()
+    {
+        if (Initialized)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        if (!SteamAPI.Init())
+        {
+            Debug.LogError("Can't initialize Steam!.");
+            return;
+        }
+
+        Initialized = true;
+        
+        Debug.Log("Steam initialized.");
+        Debug.Log("Steam User: " + SteamFriends.GetPersonaName());
+    }
+    
+    public void UnlockAchievement(string achievementID)
+    {
+        if (!Initialized)
+        {
+            return;
+        }
+
+        bool success = SteamUserStats.SetAchievement(achievementID);
+
+        if (success)
+        {
+            SteamUserStats.StoreStats();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Initialized)
+        {
+            SteamAPI.Shutdown();
+            Initialized = false;
+        }
+    }
+    
+    // To unlock the success : SteamManager.Instance.UnlockAchievement("LAMAPADRIE_SUCCESS_1");
+}
